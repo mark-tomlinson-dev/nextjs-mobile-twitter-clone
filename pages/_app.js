30304145095981f05c2c182c2224f195e0b61b9d
Mark Tomlinson <mark.tomlinson3@gmail.com>
2020-11-04 21:33:47 +1100

    init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <header className="flex p-4 border-b">
+        <img className="w-4 h-4" src="https://pbs.twimg.com/profile_images/1297446806482112512/4nRYRRB0_400x400.jpg" alt=""/>
+        <p className="text-lg ml-6">Lastest Tweets</p>
+      </header>
+      <Component {...pageProps} />
+    </div>
+  
+  
+  )
 }
 
 export default MyApp
